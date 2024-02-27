ae4bc6438d091a17949c1d61340500ca0c228f03
BeatACVR <75857504+BeatACVR@users.noreply.github.com>
2024-02-26 20:26:52 -0600

    v1.0.2 with mac support

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# RainbowIcon
+# Rainbow Icon Mod
 
-This is where she makes a mod.
+- This mod will change the color of the icons in the game to a rainbow color.
+
+- This mod was made by [SaumonDeLuxe](https://github.com/shadowforce78)
+
+- You can enable or disable the mod in the settings.
+
+- You can also change the speed of the rainbow effect in the settings.
+
+- You can also change the saturation of the rainbow effect in the settings.
+
+# Credits
+
+- Thanks [availax](https://linktr.ee/availaxmusic) for the new mod icon.
+- Thanks [beat](https://github.com/BeatACVR) for v1.0.2.
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include <Geode/Geode.hpp>
-
 #include <Geode/modify/PlayLayer.hpp>
-
 #include <chrono>
 
 using namespace geode::prelude;
@@ -9,123 +7,123 @@ using namespace std::chrono;
 
 void HSVtoRGB(float &fR, float &fG, float &fB, float &fH, float &fS, float &fV)
 {
-	float fC = fV * fS; // Chroma
-	float fHPrime = fmod(fH / 60.0, 6);
-	float fX = fC * (1 - fabs(fmod(fHPrime, 2) - 1));
-	float fM = fV - fC;
+    float fC = fV * fS; // Chroma
+    float fHPrime = fmod(fH / 60.0, 6);
+    float fX = fC * (1 - fabs(fmod(fHPrime, 2) - 1));
+    float fM = fV - fC;
 
-	if (0 <= fHPrime && fHPrime < 1)
-	{
-		fR = fC;
-		fG = fX;
-		fB = 0;
-	}
-	else if (1 <= fHPrime && fHPrime < 2)
-	{
-		fR = fX;
-		fG = fC;
-		fB = 0;
-	}
-	else if (2 <= fHPrime && fHPrime < 3)
-	{
-		fR = 0;
-		fG = fC;
-		fB = fX;
-	}
-	else if (3 <= fHPrime && fHPrime < 4)
-	{
-		fR = 0;
-		fG = fX;
-		fB = fC;
-	}
-	else if (4 <= fHPrime && fHPrime < 5)
-	{
-		fR = fX;
-		fG = 0;
-		fB = fC;
-	}
-	else if (5 <= fHPrime && fHPrime < 6)
-	{
-		fR = fC;
-		fG = 0;
-		fB = fX;
-	}
-	else
-	{
-		fR = 0;
-		fG = 0;
-		fB = 0;
-	}
+    if (0 <= fHPrime && fHPrime < 1)
+    {
+        fR = fC;
+        fG = fX;
+        fB = 0;
+    }
+    else if (1 <= fHPrime && fHPrime < 2)
+    {
+        fR = fX;
+        fG = fC;
+        fB = 0;
+    }
+    else if (2 <= fHPrime && fHPrime < 3)
+    {
+        fR = 0;
+        fG = fC;
+        fB = fX;
+    }
+    else if (3 <= fHPrime && fHPrime < 4)
+    {
+        fR = 0;
+        fG = fX;
+        fB = fC;
+    }
+    else if (4 <= fHPrime && fHPrime < 5)
+    {
+        fR = fX;
+        fG = 0;
+        fB = fC;
+    }
+    else if (5 <= fHPrime && fHPrime < 6)
+    {
+        fR = fC;
+        fG = 0;
+        fB = fX;
+    }
+    else
+    {
+        fR = 0;
+        fG = 0;
+        fB = 0;
+    }
 
-	fR += fM;
-	fG += fM;
-	fB += fM;
+    fR += fM;
+    fG += fM;
+    fB += fM;
 }
+
 float g = 0;
-cocos2d::_ccColor3B getRainbow(float offset)
+cocos2d::_ccColor3B getRainbow(float offset, float saturation)
 {
-	float R;
-	float G;
-	float B;
+    float R;
+    float G;
+    float B;
 
-	float hue = fmod(g + offset, 360);
-	// geode::log << hue;
-	float sat = 1;
-	float vc = 1;
-	HSVtoRGB(R, G, B, hue, sat, vc);
+    float hue = fmod(g + offset, 360);
+    float sat = saturation / 100.0;
+    float vc = 1;
+    HSVtoRGB(R, G, B, hue, sat, vc);
 
-	cocos2d::_ccColor3B out;
-	out.r = R * 255;
-	out.g = G * 255;
-	out.b = B * 255;
-	return out;
+    cocos2d::_ccColor3B out;
+    out.r = R * 255;
+    out.g = G * 255;
+    out.b = B * 255;
+    return out;
+}
+
+class $modify(PlayLayer)
+{
+    void postUpdate(float p0)
+    {
+        PlayLayer::postUpdate(p0);
+
+        float speed = Mod::get()->getSettingValue<double>("speed");
+        float saturation = Mod::get()->getSettingValue<double>("saturation");
+
+        if (g >= 360)
+        {
+            g = 0;
+        }
+        else
+        {
+            g += speed / 10;
+        }
+
+        auto rainbowColor = getRainbow(0, saturation);
+        auto rainbowColor2 = getRainbow(180, saturation);
+        bool enable = Mod::get()->getSettingValue<bool>("enable");
+
+        if (enable == true)
+        {
+            if (m_player1)
+            {
+                m_player1->setColor(rainbowColor);
+                m_player1->setSecondColor(rainbowColor);
+
+                if (m_player1->m_waveTrail)
+                {
+                    m_player1->m_waveTrail->setColor(rainbowColor);
+                }
+            }
+
+            if (m_player2)
+            {
+                m_player2->setColor(rainbowColor2);
+                m_player2->setSecondColor(rainbowColor2);
+
+                if (m_player2->m_waveTrail)
+                {
+                    m_player2->m_waveTrail->setColor(rainbowColor2);
+                }
+            }
+        }
+    }
 };
-
-class $modify(PlayLayer){
-
-	void postUpdate(float p0){
-
-		PlayLayer::postUpdate(p0);
-
-float speed = Mod::get()->getSettingValue<double>("speed");
-
-if (g >= 360)
-{
-	g = 0;
-}
-else
-{
-	g += speed / 10;
-}
-
-auto rainbowColor = getRainbow(0);
-auto rainbowColor2 = getRainbow(180);
-bool enable = Mod::get()->getSettingValue<bool>("enable");
-
-if (enable == true)
-{
-	if (m_player1)
-	{
-		m_player1->setColor(rainbowColor);
-		m_player1->setSecondColor(rainbowColor);
-
-		if (m_player1->m_waveTrail)
-		{
-			m_player1->m_waveTrail->setColor(rainbowColor);
-		}
-	}
-
-	if (m_player2)
-	{
-		m_player2->setColor(rainbowColor2);
-		m_player2->setSecondColor(rainbowColor2);
-
-		if (m_player2->m_waveTrail)
-		{
-			m_player2->m_waveTrail->setColor(rainbowColor2);
-		}
-	}
-}
-}
-}
-;
