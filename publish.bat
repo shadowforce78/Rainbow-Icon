1cffca001e35c4a423ac5234d1314d40986542d9
SaumonDeLuxe <planque.adam@gmail.com>
2025-12-02 19:33:25 +0100

    chore: Update `publish.bat` to use `geode index mods update` and check status with `geode index mods pending`.

--- publish.bat
+++ publish.bat
@@ -1,6 +1,6 @@
 cls
 @echo off
 @REM Execute the index submit command
-geode index submit create
+geode index mods update 
 @REM Message to the user
-echo Index creation submitted. Please check the status of the index creation using the command "geode index status".
+echo Index creation submitted. Please check the status of the index creation using the command "geode index mods pending".
